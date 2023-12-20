47b4d90a246c43ceacb9b2cde4c31d15dc8b6f3b
Minsun <minisundev@gmail.com>
2023-12-20 21:14:13 +0900

    Refactor: Title<500, Content<5000

--- src/main/java/com/minisun/board/post/dto/PostRequest.java
+++ src/main/java/com/minisun/board/post/dto/PostRequest.java
@@ -1,5 +1,6 @@
 package com.minisun.board.post.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PostRequest {
 
+    @Size(max = 500, message ="title should be shorter than 500")
     private String title;
+
+    @Size(max = 5000, message ="content should be shorter than 5000")
     private String content;
 
 }
