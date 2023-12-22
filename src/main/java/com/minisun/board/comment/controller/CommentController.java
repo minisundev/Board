9b9f94fdb83b1d8936b5b894446bdfa09ad26ab2
Minsun <minisundev@gmail.com>
2023-12-22 16:06:27 +0900

    Chore: injected CommentService to CommentController

--- src/main/java/com/minisun/board/comment/controller/CommentController.java
+++ src/main/java/com/minisun/board/comment/controller/CommentController.java
@@ -1,10 +1,18 @@
 package com.minisun.board.comment.controller;
 
+import com.minisun.board.comment.service.CommentService;
+import com.minisun.board.comment.service.CommentServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/comments")
 @RestController
 public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentServiceImpl commentServiceImpl){
+        this.commentService = commentServiceImpl;
+    }
 
 }
