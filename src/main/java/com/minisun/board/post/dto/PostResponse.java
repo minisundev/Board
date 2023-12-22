8aa72da1c9c882e458280181eb6fc64d135cb962
Minsun <minisundev@gmail.com>
2023-12-22 15:59:16 +0900

    Chore: added createdAt

--- src/main/java/com/minisun/board/post/dto/PostResponse.java
+++ src/main/java/com/minisun/board/post/dto/PostResponse.java
@@ -4,17 +4,22 @@ import com.minisun.board.post.entity.Post;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Getter
 @RequiredArgsConstructor
 public class PostResponse {
 
     private Long id;
     private String title;
+    private LocalDateTime createdAt;
     private String content;
     private String username;
 
     public PostResponse(Post post){
         this.id = post.getId();
+        this.createdAt = post.getCreatedAt();
         this.title = post.getTitle();
         this.content = post.getContent();
         this.username = post.getUser().getUsername();
